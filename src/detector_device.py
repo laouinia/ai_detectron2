2d6a23911ab5ca1a64c92343f71cb53bfd18b79a
|_Λ <laouinia@yahoo.com>
2024-03-22 10:39:10 -0700

    Clean up : Black formater

--- src/detector_device.py
+++ src/detector_device.py
@@ -1,5 +1,7 @@
 """Device handler module"""
-from  typing import Literal
+
+from typing import Literal
+
 import torch.cuda
 
-device: Literal['cuda', 'cpu'] = 'cuda' if torch.cuda.is_available() else 'cpu'
+device: Literal["cuda", "cpu"] = "cuda" if torch.cuda.is_available() else "cpu"
